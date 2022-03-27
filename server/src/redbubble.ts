af28b1e3ec86b9d105ae02aa9b5e7286ad447298
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2022-03-27 03:34:18 -0400

    Update puppeteer args

--- server/src/redbubble.ts
+++ server/src/redbubble.ts
@@ -1,7 +1,12 @@
 import puppeteer from 'puppeteer';
 
 export async function search(query: string, limit: number = 10) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    'args' : [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
   const page = await browser.newPage();
 
   await page.goto(`https://www.redbubble.com/shop/?query=${encodeURIComponent(query)}&iaCode=u-print-poster`);
