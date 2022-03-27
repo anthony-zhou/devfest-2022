721b9d0f86b6b7e871b4505ce59be8803c5eba66
Anthony Zhou <37421108+anthony-zhou@users.noreply.github.com>
2022-03-27 03:15:31 -0400

    Update port

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import * as redbubble from './redbubble';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello there!');
